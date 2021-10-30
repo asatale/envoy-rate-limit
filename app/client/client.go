705c16cdc87bce67de329e351021b9170f11a59d
Aniruddha Atale <aniruddha.atale@klaviyo.com>
2021-10-30 12:04:05 -0400

    Iterative changes

--- app/client/client.go
+++ app/client/client.go
@@ -11,74 +11,93 @@ import (
 	"time"
 )
 
+const (
+	STARTUP_TIMEOUT = 1 // 1 sec wait on startup before starting traffic
+	RPC_TIMEOUT     = 5 // 5 sec connection timeout
+)
+
 type clientConfig struct {
-	client_id    string
+	thread_id    string
 	server_addr  string
 	num_requests int
 }
 
 func main() {
-	addr := flag.String("addr", "localhost:50051", "Server address string")
+	addr := flag.String("addr", "0.0.0.0:50051", "Server address string")
 	reqs := flag.Int("reqs", 100, "Number of request sent by a client")
-	clients := flag.Int("clients", 1, "Number of clients")
+	num_threads := flag.Int("num_threads", 1, "Number of concurrent threads")
 	flag.Parse()
 
+	log.Printf("Waiting for servers to come up")
+	time.Sleep(STARTUP_TIMEOUT * time.Second)
+
+	log.Printf("Starting %v threads", *num_threads)
 	var wg sync.WaitGroup
-	for i := 0; i < *clients; i++ {
+	for i := 0; i < *num_threads; i++ {
 		wg.Add(1)
 		go communicate(
 			&clientConfig{
 				server_addr:  *addr,
-				client_id:    fmt.Sprintf("client-%d", i),
+				thread_id:    fmt.Sprintf("thread-%d", i),
 				num_requests: *reqs}, &wg)
 	}
 	wg.Wait()
 }
 
-func communicate(cfg *clientConfig, wg *sync.WaitGroup) {
-	defer wg.Done()
+func communicate(cfg *clientConfig, clientWg *sync.WaitGroup) {
+	defer clientWg.Done()
+	var (
+		conn       *grpc.ClientConn
+		errCnt     int64
+		errCntLock sync.Mutex
+		msgWg      sync.WaitGroup
+	)
 
 	latencies := make([]int64, cfg.num_requests)
-
-	log.Printf("Client: %s Start", cfg.client_id)
+	log.Printf("Thread: %s Started", cfg.thread_id)
 
 	conn, err := grpc.Dial(cfg.server_addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("Thread: %s could not connect: %v", cfg.thread_id, err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
 
-	var wgSync sync.WaitGroup
+	clnt := pb.NewGreeterClient(conn)
+
 	for i := 0; i < cfg.num_requests; i++ {
-		wgSync.Add(1)
-		go func(seqNum int, wgSync *sync.WaitGroup) {
+		msgWg.Add(1)
+
+		go func(seqNum int, wgSync *sync.WaitGroup, errCntLock *sync.Mutex) {
+			defer wgSync.Done()
+
 			ctx, cancel := context.WithTimeout(context.Background(),
-				time.Second*10)
+				time.Second*RPC_TIMEOUT)
 			defer cancel()
 
 			now := time.Now()
 			start_msec := now.UnixMilli()
-			r, err := c.SayHello(ctx, &pb.HelloRequest{ClientName: cfg.client_id,
+
+			_, err := clnt.SayHello(ctx, &pb.HelloRequest{ClientName: cfg.thread_id,
 				SeqNum: int32(seqNum)})
+
 			if err != nil {
-				log.Fatalf("could not greet: %v", err)
-			}
-			if int32(seqNum) != r.GetSeqNum() {
-				log.Fatalf("Invalid seqNum. Expected: %v, Received: %v",
-					seqNum, r.GetSeqNum())
+				log.Printf("RPC Error")
+				errCntLock.Lock()
+				errCnt = errCnt + 1
+				errCntLock.Unlock()
+				return
 			}
 
 			now = time.Now()
 			end_msec := now.UnixMilli()
 			latencies[seqNum] = end_msec - start_msec
-			wgSync.Done()
-		}(i, &wgSync)
+
+		}(i, &msgWg, &errCntLock)
 	}
-	wgSync.Wait()
+	msgWg.Wait()
 	minRTT, maxRTT, sum := minMaxSum(latencies)
-	log.Printf("Client: %s Done. Min RTT: %vms, Max RTT: %vms, Avg: %vms",
-		cfg.client_id, minRTT, maxRTT, sum/int64(cfg.num_requests))
+	log.Printf("Thread: %s Done. Min RTT: %vms, Max RTT: %vms, Avg: %vms, ErrRsp: %v",
+		cfg.thread_id, minRTT, maxRTT, sum/int64(cfg.num_requests), errCnt)
 }
 
 func minMaxSum(array []int64) (int64, int64, int64) {
--- app/server/server.go
+++ app/server/server.go
@@ -2,36 +2,35 @@ package main
 
 import (
 	"context"
+	"flag"
 	pb "github.com/asatale/envoy-rate-limit/app/proto/hello_world"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
 
-const (
-	port = ":50051"
-)
-
-// server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-
 	return &pb.HelloReply{ClientName: "Hello " + in.GetClientName(), SeqNum: in.GetSeqNum()}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+	addr := flag.String("addr", "0.0.0.0:50051", "Server address string")
+	flag.Parse()
+
+	log.Printf("Server listening on %v", *addr)
+	lis, err := net.Listen("tcp", *addr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to start listening: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
+	log.Printf("Server listening at %v", lis.Addr())
+
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("Failed to serve: %v", err)
 	}
 }
